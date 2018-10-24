a1346baa6477a2b7f7659656dd754fc114d67ae6
Colin Turner <colinturner34@gmail.com>
2018-10-23 22:55:01 -0400

    refactor: remove unused variable from app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { DataService } from './services/data.service';
 import { PlanetsTableComponent } from './planets-table/planets-table.component';
+import { PageHeaderComponent } from './page-header/page-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PlanetsTableComponent
+    PlanetsTableComponent,
+    PageHeaderComponent
   ],
   imports: [
     BrowserModule
