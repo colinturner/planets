ffb8371771184fab275f0120529f347178d2ec36
Colin Turner <colinturner34@gmail.com>
2018-10-24 20:41:00 -0400

    feat: implement planets table view

--- src/app/planets-table/planets-table.component.ts
+++ src/app/planets-table/planets-table.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../services/data.service';
+import { getViewData } from '@angular/core/src/render3/instructions';
 
 @Component({
   selector: 'app-planets-table',
@@ -8,17 +9,21 @@ import { DataService } from '../services/data.service';
 })
 export class PlanetsTableComponent implements OnInit {
 
-  public data: object;
+  public planets: object;
 
   constructor(
     private dataService: DataService
   ) {}
 
   ngOnInit() {
+    this.getPlanetsData();
+  }
+
+  getPlanetsData() {
     this.dataService.getAll<any[]>()
-    .subscribe((res: any[]) => {
-      this.data = res;
-      console.log(this.data);
+    .subscribe((response: any[]) => {
+      this.planets = response;
+      console.log(this.planets);
     },
     error => () => {
       console.log('Whoops, something went wrong');
